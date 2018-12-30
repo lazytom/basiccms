11292d8d0d6a3a75846dd4482e3677b179b6272c
Marcel Marchon <marcel@marchon.org>
2018-12-29 21:42:18 -0800

    created area "Pages"

--- src/BasicCMS/BasicCMSWeb/Startup.cs
+++ src/BasicCMS/BasicCMSWeb/Startup.cs
@@ -57,6 +57,11 @@ namespace BasicCMSWeb
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
+                  name: "areas",
+                  template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+                );
             });
         }
     }
