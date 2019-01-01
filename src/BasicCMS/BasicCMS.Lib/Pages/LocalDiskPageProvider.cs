7c7ae3aa84285f7348a32dabacdde39a422ad4ef
Marcel Marchon <marcel@marchon.org>
2018-12-31 20:59:49 -0800

    add hosting environment dependency add dynamic Content property

--- src/BasicCMS/BasicCMS.Lib/Pages/LocalDiskPageProvider.cs
+++ src/BasicCMS/BasicCMS.Lib/Pages/LocalDiskPageProvider.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,6 +10,13 @@ namespace BasicCMS.Lib.Pages
 {
     public class LocalDiskPageProvider : IPageProvider
     {
+        private IHostingEnvironment HostingEnvironment;
+
+        public LocalDiskPageProvider(IHostingEnvironment hostingEnvironment)
+        {
+            HostingEnvironment = hostingEnvironment;
+        }
+
         public Task<bool> ExistsAsync(string url)
         {
             return Task.FromResult(true);
@@ -14,6 +24,10 @@ namespace BasicCMS.Lib.Pages
 
         public Task<PageDetails> GetAsync(string url)
         {
+            var physicalPath = Path.Combine(HostingEnvironment.ContentRootPath, url ?? "");
+            dynamic content = new ExpandoObject();
+            content.FilePath = physicalPath;
+
             return Task.FromResult(new PageDetails()
             {
                 Url = url,
@@ -24,7 +38,8 @@ namespace BasicCMS.Lib.Pages
                     new Page() { Url = "test/page3", Title = "Page 3" },
                     new Page() { Url = "test/page4", Title = "Page 4" }
 
-                }
+                },
+                Content = content
             });
         }
 
--- src/BasicCMS/BasicCMS.Lib/Pages/PageDetails.cs
+++ src/BasicCMS/BasicCMS.Lib/Pages/PageDetails.cs
@@ -7,5 +7,6 @@ namespace BasicCMS.Lib.Pages
     public class PageDetails : Page
     {
         public IEnumerable<Page> ChildPages { get; set; }
+        public dynamic Content { get; set; }
     }
 }
